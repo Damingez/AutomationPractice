1c923b1df71fa74df3899f719589ce47c5a88016
Damingez <kowalix93@wp.pl>
2020-02-27 11:51:19 +0100

    Test for checking if calculation of total + shipping is proper

--- src/test/java/pl/stqa/pft/automation_practice/pages/SumUpPage.java
+++ src/test/java/pl/stqa/pft/automation_practice/pages/SumUpPage.java
@@ -2,6 +2,9 @@ package pl.stqa.pft.automation_practice.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class SumUpPage extends Page {
     public SumUpPage(WebDriver driver) {
@@ -14,5 +17,22 @@ public class SumUpPage extends Page {
 
     public void clickContinueShopping() {
         driver.findElement(By.cssSelector("span[title=\"Continue shopping\"]"));
+    }
+
+    public void checkTotaPricelCalc() {
+        // use this method in separate test
+        wait.until(visibilityOfElementLocated(By.cssSelector("span.ajax_block_products_total")));
+      double productPrice = getTextValueAndConvertToDouble(By.cssSelector("span.ajax_block_products_total"));
+      double shippingCost = getTextValueAndConvertToDouble(By.cssSelector("span.ajax_cart_shipping_cost"));
+      double total = getTextValueAndConvertToDouble(By.cssSelector("span.ajax_block_cart_total"));
+
+        Assert.assertEquals(productPrice + shippingCost,total);
+    }
+
+
+    private double getTextValueAndConvertToDouble(By by) {
+       String text = driver.findElement(by).getAttribute("textContent");
+       String cleanedString = text.replace("$","");
+       return Double.parseDouble(cleanedString);
     }
 }
--- src/test/java/pl/stqa/pft/automation_practice/tests/CalculationOfTotalTest.java
+++ src/test/java/pl/stqa/pft/automation_practice/tests/CalculationOfTotalTest.java
@@ -3,17 +3,18 @@ package pl.stqa.pft.automation_practice.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class addProductsToCartTest extends TestBase {
+public class CalculationOfTotalTest extends TestBase {
 
     @Test
-    public void addProducts() {
+    public void checkTotalOnSumUpPage() {
         Assert.assertTrue(app.productList().checkHeaderFromProductLists("Women"));
         app.productList().clickOnWomenToolbar();
         app.productList().clickOnProductDetailsWIthIndex(1);
         app.productPage().checkifHeaderIsPresent();
         app.productPage().chooseSize("M");
         app.productPage().addProductToCart();
-        app.sumUpPage().clickCheckout();
+        app.sumUpPage().checkTotaPricelCalc();
+
     }
 
 }
